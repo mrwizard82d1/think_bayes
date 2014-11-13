69fa9b6da64ba1ae38c7663c4911c700eb6f16b8
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-11-13 03:42:03 +0000

    Began implementing Pmf class.

--- think_bayes/pmf.py
+++ think_bayes/pmf.py
@@ -1,4 +1,5 @@
 """Models a probability mass function."""
+import fractions
 
 
 __author__ = 'l.jones'
@@ -15,6 +16,30 @@ class Pmf(object):
         """Returns the probability mass of item."""
         return self._repr[item]
 
+    def __iter__(self):
+        """Returns an iterator over the values of this instance."""
+        return iter(self._repr)
+
     def __setitem__(self, key, value):
         """Sets the probability mass of key to value."""
         self._repr[key] = value
+
+    def increase(self, value, amount):
+        """Increase the probability mass of value by amount."""
+        self._repr[value] = self._repr.get(value, 0) + amount
+
+    def multiply(self, value, amount):
+        """Multiply probability mass of value by amount."""
+        self._repr[value] *= amount
+
+    def normalize(self):
+        """Normalize the probability mass to actual probabilities."""
+
+        weight = sum(self._repr.values())
+        try:
+            factor = fractions.Fraction(1, weight)
+        except TypeError:
+            factor = 1 / weight
+        for value in self._repr:
+            self.multiply(value, factor)
+
--- think_bayes/test/test_pmf.py
+++ think_bayes/test/test_pmf.py
@@ -4,6 +4,7 @@
 __author__ = 'l.jones'
 
 
+import fractions
 import unittest
 
 from think_bayes import Pmf
@@ -15,6 +16,11 @@ class TestPmf(unittest.TestCase):
     A Pmf maps between values and probability (masses). Note that the probability mass returned for a value is
     only guaranteed to be a probability if the class has invoked the method normalize()."""
 
+    def setUp(self):
+        """Perform common set up for fixture."""
+
+        self.many_int_values_mass_map = {'trepidis': 6, 'invidiae': 67, 'videbio': 27}
+
     def test_get_no_values_raises_error(self):
         """Verify that attempting to query a probability mass when none set raises error."""
 
@@ -22,7 +28,71 @@ class TestPmf(unittest.TestCase):
 
         self.assertRaises(KeyError, cut.__getitem__, None)
 
-    def test_set_one_probability_one_set(self):
+    def test_increase_no_values_one_set_to_increase(self):
+        """Verify increasing the mass with no values increases the probability mass of value."""
+
+        cut = Pmf()
+
+        expect_mass = fractions.Fraction(28, 42)
+        value = 'fastidiosorum'
+        cut.increase(value, expect_mass)
+
+        self.assertEqual(expect_mass, cut[value])
+
+    def test_increase_one_value_one_increased(self):
+        """Verify increasing the mass with one value increases its probability mass."""
+
+        cut = Pmf()
+
+        start_mass = 80
+        expect_value = 'aeternales'
+        cut[expect_value] = start_mass
+
+        cut.increase(expect_value, 72)
+
+        self.assertEqual(start_mass + 72, cut[expect_value])
+
+    def test_normalize_one_value_probability_is_one(self):
+        """Verify that normalizing a Pmf with a single value makes probability one."""
+
+        cut = Pmf()
+
+        start_mass = 78.381141730761684
+        value = 9
+        cut[value] = start_mass
+
+        cut.normalize()
+
+        self.assertEqual(1, cut[value])
+
+    def test_normalize_many_values_pmf_probability_is_one(self):
+        """Verify that normalizing a Pmf with many values makes probability of any one."""
+
+        cut = self.create_many_value_pmf(self.many_int_values_mass_map)
+
+        cut.normalize()
+
+        probability_sum = 0
+        for value in cut:
+            probability_sum += cut[value]
+
+        self.assertEqual(1, probability_sum)
+
+    def test_normalize_many_values_pmf_normalizes_all_masses(self):
+        """Verify that normalizing a Pmf with many values normalizes all masses."""
+
+        cut = self.create_many_value_pmf(self.many_int_values_mass_map)
+
+        cut.normalize()
+
+        mass_total = sum(self.many_int_values_mass_map.values())
+        expect_normalized_map = {k: fractions.Fraction(v, mass_total)
+                                 for (k, v) in self.many_int_values_mass_map.items()}
+        for k in expect_normalized_map:
+            self.assertEqual(expect_normalized_map[k], cut[k])
+
+
+    def test_set_one_value_one_set(self):
         """Verify setting the probability mass for a single value sets that mass."""
 
         cut = Pmf()
@@ -32,13 +102,17 @@ class TestPmf(unittest.TestCase):
 
         self.assertEqual(0.5, actual_probability)
 
-    def test_set_many_probabilities_many_set(self):
+
+    def test_set_many_values_many_set(self):
         """Verify setting the probability masses forr many values sets many masses."""
 
+        cut = self.create_many_value_pmf(self.many_int_values_mass_map)
+
+        for value in self.many_int_values_mass_map:
+            self.assertEqual(self.many_int_values_mass_map[value], cut[value])
+
+    def create_many_value_pmf(self, actual_value_mass_map):
         cut = Pmf()
-        actual_value_mass_map = {'trepidis': 6, 'invidiae': 67, 'videbio': 27}
         for value in actual_value_mass_map:
             cut[value] = actual_value_mass_map[value]
-
-        for value in actual_value_mass_map:
-            self.assertEqual(actual_value_mass_map[value], cut[value])
+        return cut
