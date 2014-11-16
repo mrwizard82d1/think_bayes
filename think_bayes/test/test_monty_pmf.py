7b7804e2453a64c5859635099af5ae46255b6fa7
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-11-16 03:38:26 +0000

    Extracted common base class from MontyPmf.

--- think_bayes/test/test_monty_pmf.py
+++ think_bayes/test/test_monty_pmf.py
@@ -1,15 +1,17 @@
 """Defines the unit tests for the MontyPmf class."""
-import fractions
 
+from abc import abstractmethod
+import fractions
 import unittest
+
 from think_bayes import Pmf
 
 
 __author__ = 'l.jones'
 
 
-class MontyPmf(object):
-    """"Implements a class to solve the 'Monty Hall' problem using a Pmf."""
+class MontyPmfBase(object):
+    """Specifies common services and data for solving 'Monty Hall' problems."""
 
     def __init__(self, hypotheses):
         """Construct an instance with the specified hypotheses."""
@@ -17,6 +19,25 @@ class MontyPmf(object):
         self._pmf = Pmf()
         self._pmf.add_uniform(hypotheses)
         self._pmf.normalize()
+
+    @abstractmethod
+    def likelihood(self, data, hypothesis):
+        """
+        Returns the likelihood of data given hypothesis.
+        """
+        pass
+
+    def update(self, data):
+        """Update this instance having seen data."""
+
+        for hypothesis in self._pmf.values():
+            likelihood = self.likelihood(data, hypothesis)
+            self._pmf.multiply(hypothesis, likelihood)
+        self._pmf.normalize()
+
+
+class MontyPmfRandom(MontyPmfBase):
+    """"Implements a class to solve the 'Monty Hall' problem using a Pmf with assumption that Monty selects randomly."""
 
     def likelihood(self, data, hypothesis):
         """Returns the likelihood of data given hypothesis."""
@@ -32,14 +53,6 @@ class MontyPmf(object):
         # Otherwise, Monty Hall shows the other door.
         return 1
 
-    def update(self, data):
-        """Update this instance having seen data."""
-
-        for hypothesis in self._pmf.values():
-            likelihood = self.likelihood(data, hypothesis)
-            self._pmf.multiply(hypothesis, likelihood)
-        self._pmf.normalize()
-
 
 class TestMontyPmf(unittest.TestCase):
     """Defines the unit tests for the MontyPmf class."""
@@ -48,7 +61,7 @@ class TestMontyPmf(unittest.TestCase):
         """Verify the posterior probabilities if Monty Hall picks randomly between doors that DO NOT have car."""
         # The car is behind door A, B or C.
         hypos = 'ABC'
-        cut = MontyPmf(hypos)
+        cut = MontyPmfRandom(hypos)
 
         # Monty shows door B (after you pick A)
         data = 'B'
