60e29c4529f4c990d6457c4dab7832fd5c63bffb
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2018-02-05 21:40:55 -0600

    Write Pmf.increment

--- think_bayes/thinkbayes.py
+++ think_bayes/thinkbayes.py
@@ -2,12 +2,15 @@
 Module to support "Think Bayes."
 """
 
+import collections
+
+
 class Pmf(object):
     """Models a probability mass function."""
 
     def __init__(self):
         """Constructs a default instance."""
-        self.map = {} # maps values to their probabilities
+        self.map = collections.defaultdict(int) # maps values to their probabilities
 
 
     def set(self, value, mass):
@@ -17,4 +20,7 @@ class Pmf(object):
     def probability(self, value):
         """Return the probability mass of `value`."""
         return self.map[value]
+
+    def increment(self, word, by=1):
+        self.map[word] += by
 
