83bb6b8bb0965f4e33d8d40ac6aafe75d7bff1c3
Larry Jones <larry.jones@reveal-energy.com>
2018-07-26 00:04:43 -0500

    Add additional cookie function probabilities

--- think_bayes/cookie_function.py
+++ think_bayes/cookie_function.py
@@ -3,7 +3,12 @@ import fractions
 
 def cookie_function(bowl, cookie):
     probability = {'bowl1': fractions.Fraction(1, 2),
+                   'bowl2': fractions.Fraction(1, 2),
                    ('vanilla', 'bowl1'): fractions.Fraction(30, 40),
-                   'vanilla': fractions.Fraction(50, 80)}
+                   ('vanilla', 'bowl2'): fractions.Fraction(20, 40),
+                   ('chocolate', 'bowl1'): fractions.Fraction(10, 40),
+                   ('chocolate', 'bowl2'): fractions.Fraction(20, 40),
+                   'vanilla': fractions.Fraction(50, 80),
+                   'chocolate': fractions.Fraction(30, 80)}
     result = (probability[bowl] * probability[(cookie, bowl)]) / probability[cookie]
     return result
--- think_bayes/test/test_cookie_function.py
+++ think_bayes/test/test_cookie_function.py
@@ -8,6 +8,15 @@ class TestCookieFunction(unittest.TestCase):
     def test_bow1_given_vanilla(self):
         self.assertEqual(fractions.Fraction(3, 5), think_bayes.cookie_function('bowl1', 'vanilla'))
 
+    def test_bowl2_given_vanilla(self):
+        self.assertEqual(fractions.Fraction(2, 5), think_bayes.cookie_function('bowl2', 'vanilla'))
+
+    def test_bow1_given_chocolate(self):
+        self.assertEqual(fractions.Fraction(1, 3), think_bayes.cookie_function('bowl1', 'chocolate'))
+
+    def test_bowl2_given_chocolate(self):
+        self.assertEqual(fractions.Fraction(2, 3), think_bayes.cookie_function('bowl2', 'chocolate'))
+
 
 if __name__ == '__main__':
     unittest.main()
