8343b1b245aabd826fa3abed4dac623de6cf15f1
Larry Jones <larry.jones@reveal-energy.com>
2018-08-05 13:33:03 -0500

    Write `Pmf.set`

--- think_bayes/pmf.py
+++ think_bayes/pmf.py
@@ -3,3 +3,18 @@
 
 class Pmf:
     """Models a probability mass function."""
+
+    def __init__(self):
+        self.mass = {}
+
+    def set(self, value, mass):
+        """Set the probability mass of value."""
+        try:
+            self.mass[value] = mass
+        except TypeError:
+            for v in value:
+                self.set(v, mass)
+
+    def probability(self, value):
+        """Return the probability mass for `value`."""
+        return self.mass[value]
--- think_bayes/test/test_pmf.py
+++ think_bayes/test/test_pmf.py
@@ -11,3 +11,24 @@ class TestPmf(unittest.TestCase):
         cut = think_bayes.Pmf()
 
         self.assertNotEqual(cut, None)
+
+    def test_set_new_value(self):
+        cut = think_bayes.Pmf()
+        cut.set('pernicitas', 552)
+
+        self.assertEqual(cut.probability('pernicitas'), 552)
+
+    def test_change_value(self):
+        cut = think_bayes.Pmf()
+        cut.set('pernicitas', 552)
+        cut.set('pernicitas', 551)
+
+        self.assertEqual(cut.probability('pernicitas'), 551)
+
+    def test_set_multiple_values(self):
+        cut = think_bayes.Pmf()
+        cut.set(['pernicitas', 'specit', 'medii'], 984)
+
+        self.assertEqual(cut.probability('pernicitas'), 984)
+        self.assertEqual(cut.probability('specit'), 984)
+        self.assertEqual(cut.probability('medii'), 984)
