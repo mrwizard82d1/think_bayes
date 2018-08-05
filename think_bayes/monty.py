78dee1aa84d4ee3a2483d009379d8f07aed1a996
Larry Jones <larry.jones@reveal-energy.com>
2018-08-05 17:46:31 -0500

    The Monty Hall problem using the Monty class

---
+++ think_bayes/monty.py
@@ -0,0 +1,38 @@
+"""Solve the Monty Hall problem using the Monty class.
+
+This implementation assumes that Monty Hall picks randomly between doors 'B' and
+'C' whichever **does not** have the car.
+"""
+
+
+import fractions
+
+import think_bayes
+
+
+class Monty(think_bayes.Pmf):
+    """Customize the Pmf class to solve the Monty Hall problem."""
+
+    def __init__(self, hypotheses):
+        think_bayes.Pmf.__init__(self)
+        self.set(list(hypotheses), 1)
+        self.normalize()
+        print('mass function', self.mass)
+
+    def update(self, data):
+        """Update my probabilities having observed `data`."""
+
+        for hypothesis in self.values():
+            likelihood = self.likelihood(data, hypothesis)
+            self.multiply(hypothesis, likelihood)
+        self.normalize()
+
+    @staticmethod
+    def likelihood(data, hypothesis):
+        """Return the likelihood of observing `data` if `hypothesis` is true."""
+        if hypothesis == data:
+            return 0
+        elif hypothesis == 'A':
+            return fractions.Fraction(1, 2)
+        else:
+            return 1
---
+++ think_bayes/test/test_monty.py
@@ -0,0 +1,18 @@
+"""Test the Monty class for solving the Monty Hall problem."""
+
+
+import fractions
+import unittest
+
+import think_bayes
+
+
+class TestMontyHallMonty(unittest.TestCase):
+    def test_monty_hall_random_pick_monty(self):
+        cut = think_bayes.Monty('ABC')
+
+        cut.update('B')
+
+        self.assertEqual(cut.probability('A'), fractions.Fraction(1, 3))
+        self.assertEqual(cut.probability('B'), 0)
+        self.assertEqual(cut.probability('C'), fractions.Fraction(2, 3))
