7e4986bb480231c38f4573ffbb206b58e73e3fd0
Larry Jones <larry.jones@reveal-energy.com>
2018-08-26 19:58:09 -0500

    Change implementation to eat cookies from a bowl

--- think_bayes/cookie_eating.py
+++ think_bayes/cookie_eating.py
@@ -1,9 +1,13 @@
-import fractions
-
 import think_bayes
 
 
 class CookieEating(think_bayes.Suite):
+
+    vanilla_bowl_1 = ('Vanilla', 'Bowl 1')
+    vanilla_bowl_2 = ('Vanilla', 'Bowl 2')
+    chocolate_bowl_1 = ('Chocolate', 'Bowl 1')
+    chocolate_bowl_2 = ('Chocolate', 'Bowl 2')
+
     def __init__(self):
         think_bayes.Suite.__init__(self, ['Bowl 1', 'Bowl 2'])
 
@@ -17,6 +21,33 @@ class CookieEating(think_bayes.Suite):
         result = mixture_for_hypothesis.__dict__[data.lower()]
 
         return result
+
+    def update(self, data):
+        """Update my probability mass on seeing `data`."""
+        cookie, bowl = data
+        for hypothesis in self.values():
+            likelihood = self.likelihood(cookie, hypothesis)
+            self.multiply(hypothesis, likelihood)
+
+        self.normalize()
+
+        # Update my bowls based on data
+        prior_bowl_1 = self.mixes['Bowl 1']
+        prior_bowl_2 = self.mixes['Bowl 2']
+        if cookie == 'Vanilla' and bowl == 'Bowl 1':
+            posterior_bowl_1 = Bowl(prior_bowl_1.vanilla - 1, prior_bowl_1.chocolate)
+            self.mixes = {'Bowl 1': posterior_bowl_1, 'Bowl 2': prior_bowl_2}
+        elif cookie == 'Vanilla' and bowl == 'Bowl 2':
+            posterior_bowl_2 = Bowl(prior_bowl_2.vanilla - 1, prior_bowl_2.chocolate)
+            self.mixes = {'Bowl 1': prior_bowl_1, 'Bowl 2': posterior_bowl_2}
+        elif cookie == 'Chocolate' and bowl == 'Bowl 1':
+            posterior_bowl_1 = Bowl(prior_bowl_1.vanilla, prior_bowl_1.chocolate - 1)
+            self.mixes = {'Bowl 1': posterior_bowl_1, 'Bowl 2': prior_bowl_2}
+        elif cookie == 'Chocolate' and bowl == 'Bowl 2':
+            posterior_bowl_2 = Bowl(prior_bowl_2.vanilla, prior_bowl_2.chocolate - 1)
+            self.mixes = {'Bowl 1': prior_bowl_1, 'Bowl 2': posterior_bowl_2}
+        else:
+            raise ValueError('Unrecognized cookie, "{}", or bowl, "{}".'.format(cookie, bowl))
 
 
 class Bowl:
--- think_bayes/test/test_cookie_eating.py
+++ think_bayes/test/test_cookie_eating.py
@@ -8,18 +8,34 @@ import think_bayes
 
 
 class TestCookieEating(unittest.TestCase):
-    def test_vanilla(self):
+    def test_vanilla_bowl1(self):
         cut = think_bayes.CookieEating()
 
-        cut.update('Vanilla')
+        cut.update(think_bayes.CookieEating.vanilla_bowl_2)
 
         self.assertEqual(cut.probability('Bowl 1'), fractions.Fraction(3, 5))
         self.assertEqual(cut.probability('Bowl 2'), fractions.Fraction(2, 5))
 
-    def test_chocolate(self):
+    def test_vanilla_bowl2(self):
         cut = think_bayes.CookieEating()
 
-        cut.update('Chocolate')
+        cut.update(think_bayes.CookieEating.vanilla_bowl_1)
+
+        self.assertEqual(cut.probability('Bowl 1'), fractions.Fraction(3, 5))
+        self.assertEqual(cut.probability('Bowl 2'), fractions.Fraction(2, 5))
+
+    def test_chocolate_bowl1(self):
+        cut = think_bayes.CookieEating()
+
+        cut.update(think_bayes.CookieEating.chocolate_bowl_2)
+
+        self.assertEqual(cut.probability('Bowl 1'), fractions.Fraction(1, 3))
+        self.assertEqual(cut.probability('Bowl 2'), fractions.Fraction(2, 3))
+
+    def test_chocolate_bowl2(self):
+        cut = think_bayes.CookieEating()
+
+        cut.update(think_bayes.CookieEating.chocolate_bowl_1)
 
         self.assertEqual(cut.probability('Bowl 1'), fractions.Fraction(1, 3))
         self.assertEqual(cut.probability('Bowl 2'), fractions.Fraction(2, 3))
