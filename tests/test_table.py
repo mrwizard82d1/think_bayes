a9b6e2e8bb3d77c885d0c35725946eeb9c04918c
Larry Jones <larry.jones@reveal-energy.com>
2018-10-27 17:48:28 -0500

    Monty Hall variation: always pick door B

--- tests/test_table.py
+++ tests/test_table.py
@@ -33,7 +33,7 @@ class TestMontyHallTableRandomChooseAvailable(unittest.TestCase):
     # If the car is behind door 'a', Monty Hall can safely pick either door 'b' or 'c'. Assume he picks one at
     # random. Therefore, the likelihood of picking door 'b' is 1/2. If the car is behind door 'b', the likelihood of
     # Monty Hall picking door 'b' is 0. Finally, if the car is behind door 'c', the likelihood of Monty Hall picking
-    # door 'b' is is 1. (He cannot pick door 'c'.)
+    # door 'b' is 1. (He cannot pick door 'c'.)
 
     table_data = {'a': think_bayes.Table.Row(fractions.Fraction(1, 3), fractions.Fraction(1, 2)),
                   'b': think_bayes.Table.Row(fractions.Fraction(1, 3), 0),
@@ -53,4 +53,35 @@ class TestMontyHallTableRandomChooseAvailable(unittest.TestCase):
         sut = think_bayes.Table(self.table_data)
 
         self.assertEqual(fractions.Fraction(2, 3), sut.posterior('c'))
+
+
+class TestMontyHallTableRandomChooseBIfPossible(unittest.TestCase):
+    # The hypotheses, 'a', 'b', and 'c', model the situation in which the car is behind door named by the hypothesis.
+    # For example, hypothesis 'a' means that car is *actually* behind door 'a'.
+    #
+    # To calculate the likelihoods, given that you have picked door 'a', we reason as follows:
+    #
+    # If the car is behind door 'a', Monty Hall can safely pick either door 'b' or 'c'; therefore,
+    # he picks 'b' with probability 1. If the car is behind door 'b', the likelihood of Monty Hall picking door 'b' is
+    # 0. Finally, if the car is behind door 'c', the likelihood of Monty Hall picking door 'b' is also 1. (He picks
+    # door 'b' if he can.)
+
+    table_data = {'a': think_bayes.Table.Row(fractions.Fraction(1, 3), 1),
+                  'b': think_bayes.Table.Row(fractions.Fraction(1, 3), 0),
+                  'c': think_bayes.Table.Row(fractions.Fraction(1, 3), 1)}
+
+    def test_monty_hall_stay_with_a(self):
+        sut = think_bayes.Table(self.table_data)
+
+        self.assertEqual(fractions.Fraction(1, 2), sut.posterior('a'))
+
+    def test_monty_hall_cannot_pick_b(self):
+        sut = think_bayes.Table(self.table_data)
+
+        self.assertEqual(0, sut.posterior('b'))
+
+    def test_monty_hall_change_to_c(self):
+        sut = think_bayes.Table(self.table_data)
+
+        self.assertEqual(fractions.Fraction(1, 2), sut.posterior('c'))
 
