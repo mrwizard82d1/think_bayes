529e5ff47057c43b0b8727befabd849d1a4a10ec
Larry Jones <larry.jones@reveal-energy.com>
2019-05-27 20:01:17 -0500

    Rename tests (prepare for diachronic)

--- tests/test_bayes.py
+++ tests/test_bayes.py
@@ -11,14 +11,14 @@ class TestBayes(unittest.TestCase):
     def test_smoke(self):
         self.assertEqual(2 + 2, 4)
 
-    def test_vanilla_bowl_1(self):
+    def test_bayes_equation_vanilla_bowl_1(self):
         prob_cookie_from_bowl_1 = fractions.Fraction(1, 2)
         prob_vanilla_cookie_from_bowl_1 = fractions.Fraction(3, 4)
         prob_vanilla_cookie_from_either_bowl = fractions.Fraction(5, 8)
         self.assertEqual(think_bayes.bayes_equation(prob_cookie_from_bowl_1, prob_vanilla_cookie_from_bowl_1,
                                                     prob_vanilla_cookie_from_either_bowl), fractions.Fraction(3, 5))
 
-    def test_vanilla_bowl_2(self):
+    def test_bayes_equation_vanilla_bowl_2(self):
         prob_cookie_from_bowl_2 = fractions.Fraction(1, 2)
         prob_vanilla_cookie_from_bowl_2 = fractions.Fraction(1, 2)
         prob_vanilla_cookie_from_either_bowl = fractions.Fraction(5, 8)
