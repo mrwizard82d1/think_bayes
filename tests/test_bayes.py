68534e753c0b9b399dac45d9a9cbee7c9fd857ac
Larry Jones <larry.jones@reveal-energy.com>
2019-05-27 20:42:02 -0500

    Reduce common code to prepare next tests

--- tests/test_bayes.py
+++ tests/test_bayes.py
@@ -8,22 +8,23 @@ import unittest
 
 class TestBayes(unittest.TestCase):
 
+    def setUp(self):
+        self.prob_bowl_1 = fractions.Fraction(1, 2)
+        self.prob_bowl_2 = fractions.Fraction(1, 2)
+        self.prob_vanilla_bowl_1 = fractions.Fraction(3, 4)
+        self.prob_vanilla_bowl_2 = fractions.Fraction(1, 2)
+        self.prob_vanilla_either_bowl = fractions.Fraction(5, 8)
+
     def test_smoke(self):
         self.assertEqual(2 + 2, 4)
 
     def test_bayes_equation_vanilla_bowl_1(self):
-        prob_cookie_from_bowl_1 = fractions.Fraction(1, 2)
-        prob_vanilla_cookie_from_bowl_1 = fractions.Fraction(3, 4)
-        prob_vanilla_cookie_from_either_bowl = fractions.Fraction(5, 8)
-        self.assertEqual(think_bayes.bayes_equation(prob_cookie_from_bowl_1, prob_vanilla_cookie_from_bowl_1,
-                                                    prob_vanilla_cookie_from_either_bowl), fractions.Fraction(3, 5))
+        self.assertEqual(think_bayes.bayes_equation(self.prob_bowl_1, self.prob_vanilla_bowl_1,
+                                                    self.prob_vanilla_either_bowl), fractions.Fraction(3, 5))
 
     def test_bayes_equation_vanilla_bowl_2(self):
-        prob_cookie_from_bowl_2 = fractions.Fraction(1, 2)
-        prob_vanilla_cookie_from_bowl_2 = fractions.Fraction(1, 2)
-        prob_vanilla_cookie_from_either_bowl = fractions.Fraction(5, 8)
-        self.assertEqual(think_bayes.bayes_equation(prob_cookie_from_bowl_2, prob_vanilla_cookie_from_bowl_2,
-                                                    prob_vanilla_cookie_from_either_bowl), fractions.Fraction(2, 5))
+        self.assertEqual(think_bayes.bayes_equation(self.prob_bowl_2, self.prob_vanilla_bowl_2,
+                                                    self.prob_vanilla_either_bowl), fractions.Fraction(2, 5))
 
 
 if __name__ == '__main__':
